509ef9136f41216b311440504b604d4a9522acc3
maxmellberg5 <mackemax05@gmail.com>
2026-05-04 14:47:05 +0200

    blinky

--- main.c
+++ main.c
@@ -1,30 +1,30 @@
 #include "gd32vf103.h"
 #include "lcd.h"
 #include "max301.h"
+#include <stdio.h>
 
 #include "gd32vf103_i2c.h"
 #include "gd32vf103_gpio.h"
 #include "gd32vf103_rcu.h"
 
-#include "max301.h"
+#define IR_THRESHOLD 50000
+
+#include "gd32vf103.h"
+#include "gd32vf103_gpio.h"
+#include "gd32vf103_rcu.h"
 
 int main(void)
 {
-    max301init();
-    max30102_wakeup();
+    rcu_periph_clock_enable(RCU_GPIOC);
+
+    gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_13);
 
     while(1)
     {
-        if(data_ready)
-        {
-            data_ready = 0;
+        gpio_bit_reset(GPIOC, GPIO_PIN_13); // låg
+        for(volatile int i = 0; i < 1000000; i++);
 
-            uint32_t red_value;
-            uint32_t ir_value;
-
-            max30102_read_fifo(&red_value, &ir_value);
-
-            printf("RED: %lu IR: %lu\r\n", red_value, ir_value);
-        }
+        gpio_bit_set(GPIOC, GPIO_PIN_13);   // hög
+        for(volatile int i = 0; i < 1000000; i++);
     }
 }
